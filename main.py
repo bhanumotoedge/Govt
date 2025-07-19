90ba5b698e82e3efac3ba4b949bc99dff6ebeb3d
bhanumotoedge <bhanumotoedge@gmail.com>
2025-07-19 06:44:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from quiz_fetcher import fetch_quiz
-from config import DISCORD_BOT_TOKEN
+from config import BOT_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
